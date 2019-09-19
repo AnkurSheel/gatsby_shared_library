b57b0012808d5c2986397e88c221de865882faa5
Ankur Sheel <ankur.sheel@gmail.com>
2019-09-20 06:59:47 +1200

    build: add changelog and git to semantic release

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    branch: 'master',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/npm',
+        [
+            '@semantic-release/git',
+            {
+                assets: ['package.json', 'CHANGELOG.md'],
+                message: 'chore(New Release): Version ${nextRelease.version} \n\n${nextRelease.notes}', // eslint-disable-line no-template-curly-in-string
+            },
+        ],
+    ],
+};
