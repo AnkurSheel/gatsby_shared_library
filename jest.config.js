3a7341be0ab9d33c287dedf4b7af50de46a2ca9a
Ankur Sheel <ankur.sheel@gmail.com>
2019-09-26 15:04:39 +1200

    chore: add  '@testing-library/jest-dom/extend-expect'

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,11 @@ module.exports = {
     // Runs special logic, such as cleaning up components
     // when using React Testing Library and adds special
     // extended assertions to Jest
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', 'jest-axe/extend-expect'],
+    setupFilesAfterEnv: [
+        '@testing-library/jest-dom/extend-expect',
+        'jest-axe/extend-expect',
+        '@testing-library/jest-dom/extend-expect',
+    ],
 
     // Test spec file resolution pattern
     // Matches parent folder `__tests__` and filename
