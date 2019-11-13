44fae848bda797683173c9ab653bc7d0047af68b
Ankur Sheel <ankur.sheel@gmail.com>
2019-11-13 20:44:34 +1300

    build: use ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,17 @@
 module.exports = {
-    // The root of your source code, typically /src
-    // `<rootDir>` is a token Jest substitutes
-    roots: ['<rootDir>/src'],
-
-    // Jest transformations -- this adds support for TypeScript
-    // using ts-jest
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
+    globals: {
+        'ts-jest': {
+            tsConfig: './tsconfig.jest.json',
+            babelConfig: true,
+        },
     },
-
-    // Runs special logic, such as cleaning up components
-    // when using React Testing Library and adds special
-    // extended assertions to Jest
+    roots: ['<rootDir>/src'],
     setupFilesAfterEnv: [
         '@testing-library/jest-dom/extend-expect',
         'jest-axe/extend-expect',
         '@testing-library/jest-dom/extend-expect',
     ],
-
-    // Module file extensions for importing
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-
+    moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'ts', 'tsx'],
     reporters: [
         'default',
         [
@@ -30,4 +21,5 @@ module.exports = {
             },
         ],
     ],
+    preset: 'ts-jest',
 };
