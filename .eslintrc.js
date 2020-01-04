a19d1108f130ce511f8d6da27b79ec0f46a40b3b
Ankur Sheel <ankur.sheel@gmail.com>
2020-01-04 23:31:21 +1300

    build: use shared eslint config and update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,9 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-    extends: [
-        'airbnb',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
-        'plugin:prettier/recommended',
-        'prettier/react',
-        'plugin:jest/recommended',
-        'plugin:jsx-a11y/recommended',
-    ],
-    settings: {
-        react: {
-            version: 'detect',
-        },
-        'import/resolver': {
-            node: {
-                paths: ['src'],
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-            },
-        },
-    },
-    env: {
-        browser: true,
-        node: true,
-        es6: true,
-    },
-    plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
-    parserOptions: {
-        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
-    },
+    extends: ['@codinators/eslint-config', 'plugin:jest/recommended'],
     rules: {
-        'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        'react/jsx-filename-extension': [
-            1,
-            {
-                extensions: ['jsx', 'tsx'],
-            },
-        ],
         'import/no-extraneous-dependencies': [
             'error',
             { devDependencies: ['**/*.stories.tsx', '**/__tests__/**/*.[jt]s?(x)'] },
         ],
     },
-    overrides: [
-        // Override some TypeScript rules just for .js files
-        {
-            files: ['*.js'],
-            rules: {
-                '@typescript-eslint/no-var-requires': 'off', //
-                '@typescript-eslint/camelcase': 'off',
-            },
-        },
-    ],
 };
