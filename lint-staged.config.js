637b9f98418f6a0250f4003dfcc1b519faadb725
Ankur Sheel <ankur.sheel@gmail.com>
2020-01-04 23:22:57 +1300

    chore: run prettier before eslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    '*.+(js|jsx|ts|tsx)': ['eslint --fix'],
     '*.+(js|json|yml|yaml|css|less|scss|ts|tsx|md|mdx|graphql)': ['prettier --write', 'git add'],
+    '*.+(js|jsx|ts|tsx)': ['eslint --fix'],
 };
