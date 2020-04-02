af729c20e2e12d4a69614cdfd90e66b9ec8c9845
Ankur Sheel <ankur.sheel@gmail.com>
2020-04-02 16:35:45 +1300

    chore: fix broken build

--- src/02-components/quote/__tests__/quote.spec.tsx
+++ src/02-components/quote/__tests__/quote.spec.tsx
@@ -13,7 +13,7 @@ describe('<Quote />', () => {
      */
     it('should render quote if author is missing', async () => {
         const { getByText } = render(<Quote quote={quote} />);
-        const quoteElement = getByText(content => {
+        const quoteElement = getByText((content) => {
             return content === quote;
         });
         expect(quoteElement).not.toBeNull();
@@ -21,7 +21,7 @@ describe('<Quote />', () => {
 
     it('should not render author if author is missing', async () => {
         const { queryByText } = render(<Quote quote={quote} author={author} />);
-        const authorElement = queryByText(content => {
+        const authorElement = queryByText((content) => {
             return content === author;
         });
         expect(authorElement).toBeNull();
@@ -29,10 +29,10 @@ describe('<Quote />', () => {
 
     it('should render quote and author', async () => {
         const { getByText } = render(<Quote quote={quote} author={author} />);
-        const quoteElement = getByText(content => {
+        const quoteElement = getByText((content) => {
             return content === quote;
         });
-        const authorElement = getByText(content => {
+        const authorElement = getByText((content) => {
             return content === `- ${author}`;
         });
         expect(quoteElement).not.toBeNull();
