d2850abc2cbad0b8648e51ccf5a1b7d38c389aaa
Ankur Sheel <ankur.sheel@gmail.com>
2020-04-02 17:58:18 +1300

    feat: allow overriding of quote colours

--- src/02-components/quote/index.stories.tsx
+++ src/02-components/quote/index.stories.tsx
@@ -4,6 +4,14 @@ import React from 'react';
 import Quote from '.';
 
 const stories = storiesOf('Quote', module);
-stories.add('With Author', () => <Quote author={text('author', 'Author')} quote={text('quote', 'Enter a quote.')} />);
+stories.add('With Author', () => (
+    <Quote
+        author={text('author', 'Author')}
+        quote={text('quote', 'Enter a quote.')}
+        quoteColor={text('quote colour', '')}
+        authorColor={text('author colour', '')}
+        borderColor={text('border colour', '')}
+    />
+));
 
 stories.add('Without Author', () => <Quote quote={text('quote', 'Enter a quote.')} />);
--- src/02-components/quote/index.tsx
+++ src/02-components/quote/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
 
-const useStyles = createUseStyles({
+type QuoteProps = {
+    quote: string;
+    author?: string;
+    quoteColor?: string;
+    borderColor?: string;
+    authorColor?: string;
+};
+
+const useStyles = createUseStyles<QuoteProps>({
     root: {
         display: 'flex',
         justifyContent: 'center',
         marginTop: '1rem',
     },
     container: {
-        borderLeft: '8px solid #78C0A8',
+        borderLeftColor: (props: QuoteProps) => props.borderColor || '#78C0A8',
+        borderLeftStyle: 'solid',
+        borderLeftWidth: '0.5em',
         maxWidth: '40ch',
         fontSize: '1.5rem',
         padding: '0.5em 1em',
@@ -16,34 +26,29 @@ const useStyles = createUseStyles({
 
     blockquote: {
         fontStyle: 'italic',
-        color: '#555555',
+        color: (props: QuoteProps) => props.quoteColor || '#555555',
         '&:before': {
             content: '"\\201C"',
             margin: '0 0.25em 0 -.75em',
-            color: '#316a57',
+            color: (props: QuoteProps) => props.quoteColor || '#555555',
         },
         '&:after': {
             content: '"\\201D"',
             margin: '0',
-            color: '#316a57',
+            color: (props: QuoteProps) => props.quoteColor || '#555555',
         },
     },
     author: {
-        color: '#333333',
+        color: (props: QuoteProps) => props.authorColor || '#333333',
         fontstyle: 'normal',
         fontWeight: 'bold',
         textAlign: 'right',
     },
 });
 
-type QuoteProps = {
-    quote: string;
-
-    author?: string;
-};
 export const Quote = (props: QuoteProps) => {
     const { quote, author } = props;
-    const classes = useStyles();
+    const classes = useStyles(props);
 
     return (
         <div className={classes.root}>
